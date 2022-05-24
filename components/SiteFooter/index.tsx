2dc347f67b976b539aaaa15dcaac8fc6bb1f8b3b
Ye Joo Park <subwaymatch@gmail.com>
2022-05-23 21:55:36 -0500

    Add about page

--- components/SiteFooter/index.tsx
+++ components/SiteFooter/index.tsx
@@ -6,43 +6,17 @@ import { FaGithub } from "react-icons/fa";
 export default function SiteFooter() {
   return (
     <section className={styles.footer}>
-      <Container>
-        <Row>
-          <Col lg={4} md={12}>
-            <Link href="/">
-              <a>Logo</a>
-            </Link>
-          </Col>
-
-          <Col lg={4} md={12}>
-            <div className={styles.linksBox}>
-              <h2>Links</h2>
-
-              <ul>
-                <li>
-                  <Link href="/python-playground/untitled">
-                    Python Playground (Experimental)
-                  </Link>
-                </li>
-                <li>
-                  <a href="https://cscircles.cemc.uwaterloo.ca/visualize">
-                    Visualize Code Execution
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </Col>
-
-          <Col lg={4} md={12}>
-            <div className={styles.sponsorBox}>Sponsor Box</div>
-          </Col>
-        </Row>
-
+      <Container fluid>
         <Row>
           <Col>
             <div className={styles.bottomBox}>
               <Row>
-                <Col xs={10}>University of Illinois at Urbana-Champaign</Col>
+                <Col xs={10}>
+                  <Link href="https://www.centerforanalytics.giesbusiness.illinois.edu/">
+                    The University of Illinois-Deloitte Foundation Center for
+                    Business Analytics
+                  </Link>
+                </Col>
 
                 <Col xs={2}>
                   <div className={styles.iconLinks}>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,7 @@
 import Layout from "components/Layout";
 import { Container, Row, Col } from "react-bootstrap";
-import styles from "styles/pages/notes/common.module.scss";
+import styles from "styles/pages/about.module.scss";
+import Image from "next/image";
 
 export default function AboutPage() {
   return (
@@ -9,13 +10,50 @@ export default function AboutPage() {
         <Container fluid>
           <Row>
             <Col>
-              <h1 className="pageTitle">About</h1>
+              <p>
+                Analytics Open Learning is a free learning platform where anyone
+                can learn Python, SQL, and other data analytics-related topics.
+                All materials are offered for free to faculty, students, and
+                anyone else who desires to improve their knowledge of data
+                analytics thanks to an endowment from the{" "}
+                <a href="https://www2.deloitte.com/us/en/pages/about-deloitte/topics/deloitte-foundation.html">
+                  Deloitte Foundation
+                </a>{" "}
+                and its partners.
+              </p>
             </Col>
           </Row>
+        </Container>
 
+        <div className={styles.pageImageWrapper}>
+          <Image
+            src="/images/analytics-open-learning-about-graphic.png"
+            width={3000}
+            height={552}
+            alt=""
+          />
+        </div>
+
+        <Container fluid>
           <Row>
             <Col>
-              <p>Analytics Open Learning provides a </p>
+              <p>
+                If you're interested in other leading-edge business analytics
+                curriculum, please check out the{" "}
+                <a href="https://www.centerforanalytics.giesbusiness.illinois.edu/">
+                  University of Illinois-Deloitte Foundation Center for Business
+                  Analytics
+                </a>
+                .
+              </p>
+
+              <p>
+                This website and content have been created by{" "}
+                <a href="https://giesbusiness.illinois.edu/profile/ye-joo-park">
+                  Ye Joo Park
+                </a>
+                .
+              </p>
             </Col>
           </Row>
         </Container>
